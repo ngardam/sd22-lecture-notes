950a658b17d3abbd49cb6dfcf2b0092c9ef22fe9
ngardam <ngardam@gmail.com>
2022-06-27 09:52:13 -0400

    dom lesson 6-27-22

---
+++ dom-lesson/app.js
@@ -0,0 +1,33 @@
+        function sayName(name) {
+            alert(`Hello, ${name}`);
+
+            
+        }
+
+        sayName('Nick')
+
+
+        console.log(document)  // * document is page
+
+        console.log(window)  // * window is the browser itself
+
+        // *DOM
+
+        /*
+            Data representation of the objects that comprise the structure and content of a document on the web.
+
+            Basic Structure:
+                -Document
+                    -HTML
+                        -Head
+                            -Meta tags
+                            -Link tags
+                            -Title tag
+                        -Body
+                            -Nav
+                            -Header
+                            -Divs
+                            -H1's, imgs, aside's and so on...
+                            -And any of their attributes
+        */
+
