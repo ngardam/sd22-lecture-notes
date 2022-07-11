8ce26245d2af27bad4b36da0482a500dd53eda29
ngardam <ngardam@gmail.com>
2022-07-10 22:19:40 -0400

    lectures week 5

--- api-lesson/app.js
+++ api-lesson/app.js
@@ -18,3 +18,56 @@
         * & => divides multiple key/value pairs
         * # => Anchor sends up somewhere on the page
 */
+
+/* 
+
+let params = new URLSearchParams(document.location.search);
+    * URLSearchParams is an INTERFACE
+let name = params.get("name");
+
+*/
+
+
+// * API
+
+/* 
+    *Application Programming Interface
+
+    A way for a client to communicate with the server.
+
+    Allows for the http request and response lifecycle.
+*/
+
+
+// FetchAPI
+
+//Fetch with resolvers
+/*
+    1 Fetch your resource which returns a promise
+    2 use a .then resolver to catch fetch's return and assign to a parameter.
+    3 Use .json() on response to finish reading entire ReadableStream
+    4 chain another .then resolver to catch read
+*/
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => response.json())
+    .then(data => console.log(data.title))
+    .catch(error => console.log(error))
+
+
+// Top Level Await 
+// This is like not always working
+
+// let response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+// console.log(response);
+
+async function getData() {
+    
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    let data = await response.json()
+    console.log(data);
+}
+
+getData()
+
+
